e9cce2275d05458c1cc660974441b1dfadf393a2
JORGE ARMANDO GUZMAN FLORES <A01208480@itesm.mx>
2018-04-30 16:51:00 -0500

    push 1

---
+++ www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('pageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+	$scope.mdl = {};
+
+}])
+   
+.controller('page2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+		$scope.user = $stateParams.user;
+ 		$scope.password = $stateParams.password;
+ 		$scope.presence = 1;
+ 		$scope.ligth = 1;
+ 		$scope.door = 1;
+
+}])
+ 
